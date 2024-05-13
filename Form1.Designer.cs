29e4f01917b0e5fc178a4b7f180a6575c835a4c0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-13 14:43:11 +0700

    update:Add->CreateFormApp

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace QuanLyLop
+{
+    partial class QLLop_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MaLop_tb = new System.Windows.Forms.TextBox();
+            this.TenLop_tb = new System.Windows.Forms.TextBox();
+            this.TenGV_tb = new System.Windows.Forms.TextBox();
+            this.SLHS_tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ten_lb = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Them_btn = new System.Windows.Forms.Button();
+            this.Xoa_btn = new System.Windows.Forms.Button();
+            this.Sua_btn = new System.Windows.Forms.Button();
+            this.QLSV_btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.QuanLyLop_gridView = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.QuanLyLop_gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MaLop_tb
+            // 
+            this.MaLop_tb.Location = new System.Drawing.Point(192, 32);
+            this.MaLop_tb.Name = "MaLop_tb";
+            this.MaLop_tb.Size = new System.Drawing.Size(194, 22);
+            this.MaLop_tb.TabIndex = 1;
+            // 
+            // TenLop_tb
+            // 
+            this.TenLop_tb.Location = new System.Drawing.Point(192, 79);
+            this.TenLop_tb.Name = "TenLop_tb";
+            this.TenLop_tb.Size = new System.Drawing.Size(194, 22);
+            this.TenLop_tb.TabIndex = 2;
+            // 
+            // TenGV_tb
+            // 
+            this.TenGV_tb.Location = new System.Drawing.Point(192, 125);
+            this.TenGV_tb.Name = "TenGV_tb";
+            this.TenGV_tb.Size = new System.Drawing.Size(194, 22);
+            this.TenGV_tb.TabIndex = 2;
+            // 
+            // SLHS_tb
+            // 
+            this.SLHS_tb.Location = new System.Drawing.Point(240, 162);
+            this.SLHS_tb.Name = "SLHS_tb";
+            this.SLHS_tb.Size = new System.Drawing.Size(41, 22);
+            this.SLHS_tb.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // ten_lb
+            // 
+            this.ten_lb.AutoSize = true;
+            this.ten_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ten_lb.Location = new System.Drawing.Point(28, 38);
+            this.ten_lb.Name = "ten_lb";
+            this.ten_lb.Size = new System.Drawing.Size(91, 25);
+            this.ten_lb.TabIndex = 4;
+            this.ten_lb.Text = "Mã Lớp:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tên Lớp:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tên Giáo Viên:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Số Lượng Học Sinh:";
+            // 
+            // Them_btn
+            // 
+            this.Them_btn.AutoSize = true;
+            this.Them_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Them_btn.Location = new System.Drawing.Point(451, 140);
+            this.Them_btn.Name = "Them_btn";
+            this.Them_btn.Size = new System.Drawing.Size(119, 44);
+            this.Them_btn.TabIndex = 5;
+            this.Them_btn.Text = "Thêm";
+            this.Them_btn.UseVisualStyleBackColor = true;
+            // 
+            // Xoa_btn
+            // 
+            this.Xoa_btn.AutoSize = true;
+            this.Xoa_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Xoa_btn.Location = new System.Drawing.Point(757, 140);
+            this.Xoa_btn.Name = "Xoa_btn";
+            this.Xoa_btn.Size = new System.Drawing.Size(119, 44);
+            this.Xoa_btn.TabIndex = 5;
+            this.Xoa_btn.Text = "Xóa";
+            this.Xoa_btn.UseVisualStyleBackColor = true;
+            // 
+            // Sua_btn
+            // 
+            this.Sua_btn.AutoSize = true;
+            this.Sua_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sua_btn.Location = new System.Drawing.Point(610, 140);
+            this.Sua_btn.Name = "Sua_btn";
+            this.Sua_btn.Size = new System.Drawing.Size(119, 44);
+            this.Sua_btn.TabIndex = 5;
+            this.Sua_btn.Text = "Sửa";
+            this.Sua_btn.UseVisualStyleBackColor = true;
+            this.Sua_btn.Click += new System.EventHandler(this.Sua_btn_Click);
+            // 
+            // QLSV_btn
+            // 
+            this.QLSV_btn.AutoSize = true;
+            this.QLSV_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QLSV_btn.Location = new System.Drawing.Point(814, 32);
+            this.QLSV_btn.Name = "QLSV_btn";
+            this.QLSV_btn.Size = new System.Drawing.Size(184, 44);
+            this.QLSV_btn.TabIndex = 5;
+            this.QLSV_btn.Text = "QL Học Sinh";
+            this.QLSV_btn.UseVisualStyleBackColor = true;
+            this.QLSV_btn.Click += new System.EventHandler(this.Sua_btn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.QuanLyLop_gridView);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 269);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1042, 360);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin Lớp:";
+            // 
+            // QuanLyLop_gridView
+            // 
+            this.QuanLyLop_gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.QuanLyLop_gridView.Location = new System.Drawing.Point(-1, 40);
+            this.QuanLyLop_gridView.Name = "QuanLyLop_gridView";
+            this.QuanLyLop_gridView.RowHeadersWidth = 51;
+            this.QuanLyLop_gridView.RowTemplate.Height = 24;
+            this.QuanLyLop_gridView.Size = new System.Drawing.Size(1029, 314);
+            this.QuanLyLop_gridView.TabIndex = 0;
+            // 
+            // QLLop_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1053, 641);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.QLSV_btn);
+            this.Controls.Add(this.Sua_btn);
+            this.Controls.Add(this.Xoa_btn);
+            this.Controls.Add(this.Them_btn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ten_lb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SLHS_tb);
+            this.Controls.Add(this.TenGV_tb);
+            this.Controls.Add(this.TenLop_tb);
+            this.Controls.Add(this.MaLop_tb);
+            this.Name = "QLLop_Form";
+            this.Text = "QL_Lop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.QuanLyLop_gridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox MaLop_tb;
+        private System.Windows.Forms.TextBox TenLop_tb;
+        private System.Windows.Forms.TextBox TenGV_tb;
+        private System.Windows.Forms.TextBox SLHS_tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ten_lb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Them_btn;
+        private System.Windows.Forms.Button Xoa_btn;
+        private System.Windows.Forms.Button Sua_btn;
+        private System.Windows.Forms.Button QLSV_btn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView QuanLyLop_gridView;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace QuanLyLop
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView_QLHS = new System.Windows.Forms.DataGridView();
+            this.Them_btn = new System.Windows.Forms.Button();
+            this.Sua_btn = new System.Windows.Forms.Button();
+            this.Xoa_btn = new System.Windows.Forms.Button();
+            this.TenHS_lb = new System.Windows.Forms.Label();
+            this.Ho_lb = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MaHS_lb = new System.Windows.Forms.Label();
+            this.NgaySinh_lb = new System.Windows.Forms.Label();
+            this.MaLop_lb = new System.Windows.Forms.Label();
+            this.Ten_txb = new System.Windows.Forms.TextBox();
+            this.Ho_txb = new System.Windows.Forms.TextBox();
+            this.GioiTinh_txb = new System.Windows.Forms.TextBox();
+            this.MaHS_txb = new System.Windows.Forms.TextBox();
+            this.MaLop_txb = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_QLHS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView_QLHS);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 285);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1024, 306);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin Học Sinh:";
+            // 
+            // dataGridView_QLHS
+            // 
+            this.dataGridView_QLHS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_QLHS.Location = new System.Drawing.Point(7, 27);
+            this.dataGridView_QLHS.Name = "dataGridView_QLHS";
+            this.dataGridView_QLHS.RowHeadersWidth = 51;
+            this.dataGridView_QLHS.RowTemplate.Height = 24;
+            this.dataGridView_QLHS.Size = new System.Drawing.Size(1011, 273);
+            this.dataGridView_QLHS.TabIndex = 0;
+            // 
+            // Them_btn
+            // 
+            this.Them_btn.AutoSize = true;
+            this.Them_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Them_btn.Location = new System.Drawing.Point(354, 126);
+            this.Them_btn.Name = "Them_btn";
+            this.Them_btn.Size = new System.Drawing.Size(77, 35);
+            this.Them_btn.TabIndex = 1;
+            this.Them_btn.Text = "Thêm";
+            this.Them_btn.UseVisualStyleBackColor = true;
+            // 
+            // Sua_btn
+            // 
+            this.Sua_btn.AutoSize = true;
+            this.Sua_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sua_btn.Location = new System.Drawing.Point(659, 126);
+            this.Sua_btn.Name = "Sua_btn";
+            this.Sua_btn.Size = new System.Drawing.Size(77, 35);
+            this.Sua_btn.TabIndex = 1;
+            this.Sua_btn.Text = "Sửa";
+            this.Sua_btn.UseVisualStyleBackColor = true;
+            // 
+            // Xoa_btn
+            // 
+            this.Xoa_btn.AutoSize = true;
+            this.Xoa_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Xoa_btn.Location = new System.Drawing.Point(502, 126);
+            this.Xoa_btn.Name = "Xoa_btn";
+            this.Xoa_btn.Size = new System.Drawing.Size(77, 35);
+            this.Xoa_btn.TabIndex = 1;
+            this.Xoa_btn.Text = "Xóa";
+            this.Xoa_btn.UseVisualStyleBackColor = true;
+            // 
+            // TenHS_lb
+            // 
+            this.TenHS_lb.AutoSize = true;
+            this.TenHS_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TenHS_lb.Location = new System.Drawing.Point(25, 28);
+            this.TenHS_lb.Name = "TenHS_lb";
+            this.TenHS_lb.Size = new System.Drawing.Size(53, 25);
+            this.TenHS_lb.TabIndex = 2;
+            this.TenHS_lb.Text = "Tên:";
+            // 
+            // Ho_lb
+            // 
+            this.Ho_lb.AutoSize = true;
+            this.Ho_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ho_lb.Location = new System.Drawing.Point(25, 64);
+            this.Ho_lb.Name = "Ho_lb";
+            this.Ho_lb.Size = new System.Drawing.Size(43, 25);
+            this.Ho_lb.TabIndex = 2;
+            this.Ho_lb.Text = "Họ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Giới tính:";
+            // 
+            // MaHS_lb
+            // 
+            this.MaHS_lb.AutoSize = true;
+            this.MaHS_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaHS_lb.Location = new System.Drawing.Point(25, 137);
+            this.MaHS_lb.Name = "MaHS_lb";
+            this.MaHS_lb.Size = new System.Drawing.Size(124, 25);
+            this.MaHS_lb.TabIndex = 2;
+            this.MaHS_lb.Text = "Mã học sinh:";
+            // 
+            // NgaySinh_lb
+            // 
+            this.NgaySinh_lb.AutoSize = true;
+            this.NgaySinh_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NgaySinh_lb.Location = new System.Drawing.Point(289, 28);
+            this.NgaySinh_lb.Name = "NgaySinh_lb";
+            this.NgaySinh_lb.Size = new System.Drawing.Size(105, 25);
+            this.NgaySinh_lb.TabIndex = 2;
+            this.NgaySinh_lb.Text = "Ngày sinh:";
+            // 
+            // MaLop_lb
+            // 
+            this.MaLop_lb.AutoSize = true;
+            this.MaLop_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaLop_lb.Location = new System.Drawing.Point(25, 183);
+            this.MaLop_lb.Name = "MaLop_lb";
+            this.MaLop_lb.Size = new System.Drawing.Size(77, 25);
+            this.MaLop_lb.TabIndex = 2;
+            this.MaLop_lb.Text = "Mã lớp:";
+            // 
+            // Ten_txb
+            // 
+            this.Ten_txb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ten_txb.Location = new System.Drawing.Point(104, 30);
+            this.Ten_txb.Name = "Ten_txb";
+            this.Ten_txb.Size = new System.Drawing.Size(151, 27);
+            this.Ten_txb.TabIndex = 3;
+            // 
+            // Ho_txb
+            // 
+            this.Ho_txb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ho_txb.Location = new System.Drawing.Point(104, 65);
+            this.Ho_txb.Name = "Ho_txb";
+            this.Ho_txb.Size = new System.Drawing.Size(151, 27);
+            this.Ho_txb.TabIndex = 3;
+            // 
+            // GioiTinh_txb
+            // 
+            this.GioiTinh_txb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GioiTinh_txb.Location = new System.Drawing.Point(119, 101);
+            this.GioiTinh_txb.Name = "GioiTinh_txb";
+            this.GioiTinh_txb.Size = new System.Drawing.Size(51, 27);
+            this.GioiTinh_txb.TabIndex = 3;
+            // 
+            // MaHS_txb
+            // 
+            this.MaHS_txb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaHS_txb.Location = new System.Drawing.Point(155, 134);
+            this.MaHS_txb.Name = "MaHS_txb";
+            this.MaHS_txb.Size = new System.Drawing.Size(51, 27);
+            this.MaHS_txb.TabIndex = 3;
+            // 
+            // MaLop_txb
+            // 
+            this.MaLop_txb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaLop_txb.Location = new System.Drawing.Point(108, 184);
+            this.MaLop_txb.Name = "MaLop_txb";
+            this.MaLop_txb.Size = new System.Drawing.Size(51, 27);
+            this.MaLop_txb.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(400, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(225, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1049, 603);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.MaLop_txb);
+            this.Controls.Add(this.MaHS_txb);
+            this.Controls.Add(this.GioiTinh_txb);
+            this.Controls.Add(this.Ho_txb);
+            this.Controls.Add(this.Ten_txb);
+            this.Controls.Add(this.NgaySinh_lb);
+            this.Controls.Add(this.MaLop_lb);
+            this.Controls.Add(this.MaHS_lb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Ho_lb);
+            this.Controls.Add(this.TenHS_lb);
+            this.Controls.Add(this.Xoa_btn);
+            this.Controls.Add(this.Sua_btn);
+            this.Controls.Add(this.Them_btn);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Text = "QLHS_Form";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_QLHS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView_QLHS;
+        private System.Windows.Forms.Button Them_btn;
+        private System.Windows.Forms.Button Sua_btn;
+        private System.Windows.Forms.Button Xoa_btn;
+        private System.Windows.Forms.Label TenHS_lb;
+        private System.Windows.Forms.Label Ho_lb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label MaHS_lb;
+        private System.Windows.Forms.Label NgaySinh_lb;
+        private System.Windows.Forms.Label MaLop_lb;
+        private System.Windows.Forms.TextBox Ten_txb;
+        private System.Windows.Forms.TextBox Ho_txb;
+        private System.Windows.Forms.TextBox GioiTinh_txb;
+        private System.Windows.Forms.TextBox MaHS_txb;
+        private System.Windows.Forms.TextBox MaLop_txb;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
