82adf6b1b19a37337ff9385afddbe5bc10fcb109
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-25 10:36:59 +0700

    update:Add

--- Connection.cs
+++ Connection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace QuanLyLop
 {
     internal class Connection
     {
+        private static string connec = @"Data Source=DESKTOP-GGGCNOA;Initial Catalog=""Quản lí Lớp và Học Sinh"";Integrated Security=TrueData Source=DESKTOP-GGGCNOA;Initial Catalog=""Quản lí Lớp và Học Sinh"";Integrated Security=True";
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connec);
+        }
     }
 }
